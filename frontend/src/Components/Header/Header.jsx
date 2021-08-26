5f9be38f5695f2fdb63727cb0f73b25e67f7865e
prashanth <vengalaprashanth24@gmail.com>
2021-08-26 18:03:19 +0530

    Header component UI added

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 import {
   AppBar,
@@ -12,13 +13,56 @@ import {
   List,
   ListItem,
 } from "@material-ui/core";
+import HeaderButtons from "./HeaderButtons";
 
 const useStyle = makeStyles((theme) => ({
   header: {
     background: "#2874f0",
     height: 55,
   },
+  component: {
+    marginLeft: "12%",
+    lineHeight: 0,
+    color: "#FFFFFF",
+    textDecoration: "none",
+  },
+  logo: {
+    width: 75,
+  },
+  container: {
+    display: "flex",
+  },
+  subHeading: {
+    fontSize: 10,
+    fontStyle: "italic",
+  },
+  subURL: {
+    width: 10,
+    height: 10,
+    marginLeft: 4,
+  },
+  list: {
+    width: 250,
+  },
+  menuButton: {
+    display: "none",
+    [theme.breakpoints.down("sm")]: {
+      display: "block",
+    },
+  },
+  customButtons: {
+    margin: "0 5% 0 auto",
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
 }));
+
+const ToolBar = withStyles({
+  root: {
+    minHeight: 55,
+  },
+})(Toolbar);
 
 const Header = () => {
   const classes = useStyle();
@@ -29,9 +73,22 @@ const Header = () => {
   return (
     <>
       <AppBar position="fixed" className={classes.header}>
-        <Toolbar>
-          <img src={logoURL} />
-        </Toolbar>
+        <ToolBar>
+          <Box className={classes.component}>
+            <img src={logoURL} className={classes.logo} />
+            <Box className={classes.container}>
+              <Typography className={classes.subHeading}>
+                Explore{" "}
+                <Box component="span" style={{ color: "#FFE500" }}>
+                  Plus
+                </Box>
+              </Typography>
+              <img src={subURL} alt="subLogo" className={classes.subURL} />
+            </Box>
+          </Box>
+          <SearchBar />
+          <HeaderButtons />
+        </ToolBar>
       </AppBar>
     </>
   );
